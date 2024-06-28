1964fdbb80299c5e72d4d38cbf6d07c5dd55dab7
Sebastian <mrseb10@gmail.com>
2024-06-29 01:35:22 +0200

    Repository Pattern declaration and implementation

---
+++ Infrastructure/Repositories/IProductsRepository.cs
@@ -0,0 +1,11 @@
+﻿using Infrastructure.Models;
+
+namespace Infrastructure.Repositories;
+
+public interface IProductsRepository
+{
+  Task<PaginatedProducts> GetProductsAsync(int page, int pageSize);
+  Task AddProductAsync(Product product);
+  Task UpdateProductAsync(Product product);
+  Task DeleteProductAsync(Product product);
+}
